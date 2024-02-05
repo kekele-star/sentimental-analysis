6a32e02a8f68bad8a5f416133231b98b07927b74
Kekele <esinamdemanya@gmail.com>
2024-02-05 15:46:10 +0000

    updated code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from textblob import textblob
 from newspaper import Article
 
-url = ""
+url = "https://en.wikipedia.org/wiki/Computer_science"
 article = Article(url)
 
 article.download()
@@ -10,3 +10,7 @@ article.nlp()
 
 text = article.text
 print(text)
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity
+print(sentiment)
