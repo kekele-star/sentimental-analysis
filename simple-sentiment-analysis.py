c16a1dd1c66be13132c993b7b1bcda2819fa83ed
Kekele <esinamdemanya@gmail.com>
2024-06-16 00:11:48 +0000

    Update simple-sentiment-analysis.py

--- simple-sentiment-analysis.py
+++ simple-sentiment-analysis.py
@@ -1,6 +1,13 @@
 from textblob import textblob
 from newspaper import Article
 
+# url = "https://www.bbc.com/news/ul-england-london-52963555
+# article = Article(url)
+
+# article.download()
+# article.parse()
+# article.nlp()
+
 with open('text.txt', 'r') as f:
     text = f.read()
 
